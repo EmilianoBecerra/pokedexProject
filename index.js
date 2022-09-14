d0da52e20b477bb71c4415aa49211bc5efbd883d
Emiliano <emibecerra92@gmail.com>
2022-09-14 19:28:34 -0300

    micommit

---
+++ index.js
@@ -0,0 +1,23 @@
+const baseURL = ()=> {
+	let tipo = document.getElementById("parametroValue2").value;
+	let arrValue = [];
+	let parametro = document.getElementById('pokemon').value;
+	let url = "https://pokeapi.co/api/v2/pokemon/" + parametro;
+	fetch(url)
+		.then((response => response.json()))
+		.then(myJson =>{
+			let element = document.getElementById('poke-table')
+			document.getElementById("img").src = myJson.sprites.front_default;
+			arrValue.push(myJson.name);
+			arrValue.push(myJson.id);
+			arrValue.push(myJson.types[0].type.name);
+			arrValue.push(myJson.abilities[0].ability.name);
+
+			for(index in arrValue){
+				document.getElementById('parametroValue' + index).innerHTML = arrValue[index]
+			}
+
+		})
+}
+
+
